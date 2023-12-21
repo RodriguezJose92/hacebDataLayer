4897e71540a6e07514b2e66825b82536042d0aa9
Jose Rodriguez <131327988+RodriguezJose92@users.noreply.github.com>
2023-12-21 14:52:05 -0500

    Update dataLayer.js

--- dataLayer.js
+++ dataLayer.js
@@ -1,4 +1,7 @@
-if( document.querySelector('.escritorio')){
+const script = document.createElement('SCRIPT')
+script.id='MudiDataLayer'
+script.innerHTML=`
+if( document.querySelenctor('.escritorio')){
     dataLayer.push({
         eventoMudi:'Visualización de Botones',
         mudiBtns: 1
@@ -8,4 +11,10 @@ if( document.querySelector('.escritorio')){
         eventoMudi:'Visualización de Botones',
         mudiBtns: 0
     })
+}`;
+
+if( document.getElementById('MudiDataLayer')){
+    document.appendChild(script)
+}else{
+    console.log('Algo raro paso perros')
 }
